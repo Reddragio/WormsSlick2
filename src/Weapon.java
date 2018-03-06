344f9ef4243471ae431174daad5c26a43e1c161b
Reddragio <jacques.r.charnay@hotmail.fr>
2018-03-06 22:27:01 +0100

    --> Correction de bugs de visée

--- src/Weapon.java
+++ src/Weapon.java
@@ -28,15 +28,21 @@ public abstract class Weapon {
     public void init(int xw,int yw,int hitBoxLargeur,int hitBoxHauteur,int orientation){
         orientationWorms = orientation;
         if(orientationWorms==0){
+            /*x = xw - hitBoxLargeur/3;
+            y = yw - hitBoxHauteur/2 - pictureLeft.getHeight()/4;
+            demiDiago = Math.sqrt(Math.pow(pictureLeft.getWidth(),2)+Math.pow(pictureLeft.getHeight(),2))/2.0;*/
             x = xw - hitBoxLargeur/3;
             y = yw - hitBoxHauteur/2 - pictureLeft.getHeight()/4;
-            demiDiago = Math.sqrt(Math.pow(pictureLeft.getWidth(),2)+Math.pow(pictureLeft.getHeight(),2))/2.0;
+            demiDiago = pictureLeft.getWidth()/2.0;
         }
         else
         {
+            /*x = xw + hitBoxLargeur/6;
+            y = yw - hitBoxHauteur/2 - pictureRight.getHeight()/4;
+            demiDiago = Math.sqrt(Math.pow(pictureRight.getWidth(),2)+Math.pow(pictureRight.getHeight(),2))/2.0;*/
             x = xw + hitBoxLargeur/6;
             y = yw - hitBoxHauteur/2 - pictureRight.getHeight()/4;
-            demiDiago = Math.sqrt(Math.pow(pictureRight.getWidth(),2)+Math.pow(pictureRight.getHeight(),2))/2.0;
+            demiDiago = pictureRight.getWidth()/2.0;
         }
     }
 
@@ -44,20 +50,32 @@ public abstract class Weapon {
         if(orientationWorms==0){
             xCentre = x + pictureLeft.getWidth()/2;
             yCentre = y + pictureLeft.getHeight()/2;
-            xCoinDroite = (int)(demiDiago * Math.cos((-angle/180.0)*Math.PI+coinDroiteOffset+(offSetLeft/180.0)*Math.PI) + xCentre);
+            /*xCoinDroite = (int)(demiDiago * Math.cos((-angle/180.0)*Math.PI+coinDroiteOffset+(offSetLeft/180.0)*Math.PI) + xCentre);
             yCoinDroite = (int)(demiDiago * Math.sin((-angle/180.0)*Math.PI+coinDroiteOffset+(offSetLeft/180.0)*Math.PI) + yCentre);
             xTemp = 2*x + pictureLeft.getWidth() - xCoinDroite;
             yTemp = 2*y + pictureLeft.getHeight() - yCoinDroite;
+            pictureLeft.setRotation((float)(-angle+offSetLeft));
+            pictureLeft.draw(xTemp,yTemp);*/
+            xCoinDroite = (int)(demiDiago * Math.cos((-angle/180.0)*Math.PI+coinDroiteOffset) + xCentre);
+            yCoinDroite = (int)(demiDiago * Math.sin((-angle/180.0)*Math.PI+coinDroiteOffset) + yCentre);
+            xTemp = 2*x + pictureLeft.getWidth() - xCoinDroite;
+            yTemp = 2*y + pictureLeft.getHeight()/2 - yCoinDroite;
             pictureLeft.setRotation((float)(-angle+offSetLeft));
             pictureLeft.draw(xTemp,yTemp);
         }
         else{
             xCentre = x + pictureRight.getWidth()/2;
             yCentre = y + pictureRight.getHeight()/2;
-            xCoinGauche = (int)(demiDiago * Math.cos((angle/180.0)*Math.PI+coinGaucheOffset+(offSetRight/180.0)*Math.PI) + xCentre);
+            /*xCoinGauche = (int)(demiDiago * Math.cos((angle/180.0)*Math.PI+coinGaucheOffset+(offSetRight/180.0)*Math.PI) + xCentre);
             yCoinGauche = (int)(demiDiago * Math.sin((angle/180.0)*Math.PI+coinGaucheOffset+(offSetRight/180.0)*Math.PI) + yCentre);
             xTemp = 2*x - xCoinGauche;
             yTemp = 2*y + pictureRight.getHeight() - yCoinGauche;
+            pictureRight.setRotation((float)(angle+offSetRight));
+            pictureRight.draw(xTemp,yTemp);*/
+            xCoinGauche = (int)(demiDiago * Math.cos((angle/180.0)*Math.PI+coinGaucheOffset) + xCentre);
+            yCoinGauche = (int)(demiDiago * Math.sin((angle/180.0)*Math.PI+coinGaucheOffset) + yCentre);
+            xTemp = 2*x - xCoinGauche;
+            yTemp = 2*y + pictureRight.getHeight()/2 - yCoinGauche;
             pictureRight.setRotation((float)(angle+offSetRight));
             pictureRight.draw(xTemp,yTemp);
         }

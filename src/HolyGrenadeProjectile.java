ae9e263bc21b9e72f14168cbf3b260a9ed7efc2b
Paulo <paulbertrand@orange.fr>
2018-03-21 22:23:16 +0100

    add Son Holy Hand Grenade

--- src/HolyGrenadeProjectile.java
+++ src/HolyGrenadeProjectile.java
@@ -7,7 +7,7 @@ public class HolyGrenadeProjectile extends GrenadeProjectile{
        super(terrain,blockSize,tireur);
         pictureLeft = new org.newdawn.slick.Image("images/HolyGrenadeSD_left.png");
         pictureRight = new org.newdawn.slick.Image("images/HolyGrenadeSD_right.png");
-        bruitExplosion = new Sound("music/Grenade.ogg"); //Changer son
+        bruitExplosion = new Sound("music/HOLYGRENADE.wav"); //Changer son
         bruitFail = new Sound("music/failSound.ogg");
         rayonExplosion = 150;
         degat=150;
